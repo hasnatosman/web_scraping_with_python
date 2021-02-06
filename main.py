13686d90510cea01a816fdfa3da4d760cbb87de1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-06 16:04:07 +0600

    Plan for this book.

---
+++ main.py
@@ -0,0 +1,6 @@
+"""
+In this project, I wil try to cover the whole book 'Web Scraping with python' . my target is for this month is
+one lesson at least par day. That will help me out to go next. I love this things so much.
+In this book, there are almost 18 lessons . So if i complete one lesson par day, it will take just 18 days.
+
+"""
