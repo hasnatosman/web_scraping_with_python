43d2a13eac9b3d13968403564466fc731a411abd
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2021-02-20 11:27:20 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,33 @@
-"""
-In this project, I wil try to cover the whole book 'Web Scraping with python' . my target is for this month is
-one lesson at least par day. That will help me out to go next. I love this things so much.
-In this book, there are almost 18 lessons . So if i complete one lesson par day, it will take just 18 days.
+import requests
+from bs4 import BeautifulSoup
 
-"""
+result = requests.get('https://www.goal.com/en-in')  # using the requests module , we use the get function
+# print(result.status_code)
+
+# we store page content to a variable 'src'
+src = result.content
+# print(src)
+
+#  we stored the page, now we will use the Beautiful module to parse the source
+# create a BeautifulSoup object name 'soup'
+
+soup = BeautifulSoup(src, 'lxml')
+
+# here i will find all the a tag , and store it on links variable
+
+links = soup.find_all('a')
+# print(links)
+# print('\n')
+
+
+# now we will find all the text that contains 'About' on the page instead of every link
+for link in links:
+    if 'Klopp' in link.text:
+        print(link)
+        print(link.attrs['href'])
+
+# print(result.status_code)
+# using status code we see that url is ok
+
+
+# print(result.headers)
